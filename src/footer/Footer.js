4bb65addf992318431030bf1f692b8a6be8f2d95
arusz <arusimanjhbeh24@gmail.com>
2022-12-03 03:50:43 +0700

    Menambahkan Footer

---
+++ src/footer/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+function Footer() {
+    return(
+        <>
+            <footer>
+                <div className="footer-content">
+                    <h3>SMABIS@</h3>
+                </div>
+                <div className="footer-bottom">
+                    <p>Copyright &copy;2022 design by <span>SMABIS@</span></p>
+                </div>
+            </footer>
+        </>
+    )
+}
+
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import App from './App';
 import Navbar from './navbar/NavvBar';
+import Footer from './footer/Footer';
 
 import './styles/style.css';
 
@@ -11,5 +12,6 @@ root.render(
     <BrowserRouter>
         <Navbar />
         <App />
+        <Footer />
     </BrowserRouter>
 );
