09e04d8475851f05daa4c498e7fd3b9f4bf35ed8
B-RunP <brianpurnama157@gmail.com>
2022-12-11 17:46:57 +0700

    menambah fitur login

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,8 +1,16 @@
+import Brian from '../images/Brian.jpg';
+
 function AboutPage() {
     return(
-        <h1>
-            About Page
-        </h1>
+        <div className="about">
+            <div className="card-profile__about">
+                <img src={Brian} alt="John" />
+                <h1>Brian Purnama Putra</h1>
+                <p className="university-name">Universitas Duta Bangsa Surakarta</p>
+                <p>Harvard University</p>
+                <p><button className="card-button__about">Contact</button></p>
+            </div>
+        </div>
     )
 }
 
